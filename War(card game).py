02debef49a019d6b717421c749ea9903d89c0434
INF308GilbertKarugu <gkarugu@albany.edu>
2021-11-02 23:09:23 -0400

    added suits and appended deck

--- War(card game).py
+++ War(card game).py
@@ -3,4 +3,9 @@ import random
 
 
 Deck = []
-Ranks ["A","2","4","5","6","7","8","9","10","J","Q","K"]
+Ranks ["A", "2", "4", "5", "6", "7", "8", "9", "10" ,"J" ,"Q", "K"]
+Suits[ "Hearts", "Spades", "Clubs","Diamonds"]
+
+    for Rank in Ranks:
+        for Suit in Suits:
+Deck.append(Rank + " " + Suit)
