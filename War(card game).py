d5e3287199569ba74fa4a56edb11ae95bc0565bd
INF308GilbertKarugu <gkarugu@albany.edu>
2021-11-03 20:58:53 -0400

    Fixed print statement error

--- War(card game).py
+++ War(card game).py
@@ -9,5 +9,5 @@ Card_Suits = [ "Hearts", "Spades", "Clubs","Diamonds"]
 for Rank in Card_Ranks:
     for Suit in Card_Suits:
         Card_Deck.append(Rank + " " + Suit)
-        card = random.choice(Card_Deck)
-Print (Card_Deck) 
+        Card = random.choice(Card_Deck)
+        print (Card)
