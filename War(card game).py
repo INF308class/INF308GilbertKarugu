1b8730113a266f120aa820537f5016dd54b5a69d
INF308GilbertKarugu <gkarugu@albany.edu>
2021-11-03 20:39:56 -0400

    Added print statement and random choice

--- War(card game).py
+++ War(card game).py
@@ -2,10 +2,12 @@ import random
 
 
 
-Deck = []
-Ranks ["A", "2", "4", "5", "6", "7", "8", "9", "10" ,"J" ,"Q", "K"]
-Suits[ "Hearts", "Spades", "Clubs","Diamonds"]
+Card_Deck = []
+Card_Ranks = ["A", "2", "4", "5", "6", "7", "8", "9", "10" ,"J" ,"Q", "K"]
+Card_Suits = [ "Hearts", "Spades", "Clubs","Diamonds"]
 
-    for Rank in Ranks:
-        for Suit in Suits:
-Deck.append(Rank + " " + Suit)
+for Rank in Card_Ranks:
+    for Suit in Card_Suits:
+        Card_Deck.append(Rank + " " + Suit)
+        card = random.choice(Card_Deck)
+Print (Card_Deck) 
