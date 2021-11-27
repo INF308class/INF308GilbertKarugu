714225eff86fce42f622e52ac716b19fc8bb3b94
INF308GilbertKarugu <gkarugu@albany.edu>
2021-11-26 20:06:23 -0500

    added menu and menu options

--- recipe original.py
+++ recipe original.py
@@ -1,4 +1,13 @@
 def action():
     global recipes
     recipes ={}
+    while True:
+        answer = input("Select from the Menu \n"
+                       "N.) Add new recipe\n"
+                       "L.) List existing recipes\n"
+                       "R.) Read recipe\n"
+                       "Q.) Quit\n\n")
+                      
     
+                       
+                    
