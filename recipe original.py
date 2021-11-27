cd611728988bbc3ce836e267bfe063b5ddef9828
INF308GilbertKarugu <gkarugu@albany.edu>
2021-11-26 20:26:39 -0500

    Added loop for menu and recipe

--- recipe original.py
+++ recipe original.py
@@ -10,4 +10,17 @@ def action():
                       
     
                        
-                    
+         
+        if answer == "L" :
+            print(recipes.values())
+        elif answer == "N":           
+            name =str(input("\nWhat recipe would you would like to make ?  \n"))
+            ingredients= str(input("\nWhat are the ingredients for the recipe :- \n"))
+            method =str(input('\nHow is the meal prepared :- \n'))
+            recipes[name]=[name,ingredients,method]
+            print(recipes[name])
+        elif answer == "R":   
+             print(recipes)
+        elif answer == "Q":
+            break
+action()
