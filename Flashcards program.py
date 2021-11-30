60f7f90335ddf5f2ec395d1ecbc2059e15f3f088
INF308GilbertKarugu <gkarugu@albany.edu>
2021-11-29 20:46:35 -0500

    define function

---
+++ Flashcards program.py
@@ -0,0 +1,16 @@
+import random
+
+def action():
+    global Flashcards
+    Flashcards ={}
+    while True:
+        answer = input("Select from the Menu \n"
+                       "1.) Add new Flashcard question\n"
+                       "2.) List answer for Flashcard\n"
+                       "3.) Correct Answer\n"
+                       "4.) Incorrect answer\n"
+                       "Q.) Quit\n\n")
+                      
+    
+                       
+       
